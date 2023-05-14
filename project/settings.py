79b76bc0a55b5eb13b13bdb2dd749fe5d511a881
Abdulrahman_Elsharef <78035855+AbdulrahmanElsharef@users.noreply.github.com>
2023-05-14 12:54:52 +0300

    add user acccounts

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ErpApp',
     "bootstrap4",
+
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,5 @@ MEDIA_ROOT = BASE_DIR /  'media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/home'
--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
     path('', include('ErpApp.urls',namespace='home')),
 ]
