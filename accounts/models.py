3c7b9b305c80d2eb2fe1fa87042d0b20a1750533
Abdulrahman_Elsharef <78035855+AbdulrahmanElsharef@users.noreply.github.com>
2023-05-14 15:11:49 +0300

    add create_profile fun

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE,related_name='User_Profile')
+    phone=models.CharField( max_length=20,null=True,blank=True)
+    address=models.TextField(max_length=200)
+    
+    def __str__(self):
+        return str(self.user)
+    
+@receiver(post_save, sender=User)
+def creeate_profile(sender,instance,created, **kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance
+        )
+    
+
+    
